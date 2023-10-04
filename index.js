4ce5ea97b1d5da879948724a4d500531a542f6f1
clbowley <carson@metapattern.is>
2023-10-04 09:50:05 -0400

    Slowing down

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function main({ pane, glslVersion, contextID }) {
 		particleDensity: 0.10,
 		sensorDistance: 18, 
 		sensorAngle: 5.5,
-		stepSize: 1,
+		stepSize: 0.2,
 		rotationAngle: 45,
 		renderAmplitude: 0.03,
 		currentPreset: 'Fibers',
